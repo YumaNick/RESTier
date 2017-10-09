c13d9ddbc4aeb6fbfebbe74678196bdb45d610e4
Anatoly Volodko <Anatoly.V.Volodko@galaktika.by>
2017-10-09 18:50:10 +0300

    fix errors while test databases creation

--- test/ODataEndToEnd/Microsoft.OData.Service.Sample.Northwind/Models/NorthwindContext.cs
+++ test/ODataEndToEnd/Microsoft.OData.Service.Sample.Northwind/Models/NorthwindContext.cs
@@ -11,12 +11,12 @@ using System.Data.Entity;
 
 namespace Microsoft.OData.Service.Sample.Northwind.Models
 {
-    public class NorthwindContext : DbContext
-    {
-        static NorthwindContext()
-        {
-            LoadDataSource();
-        }
+	public class NorthwindContext : DbContext
+	{
+		static NorthwindContext()
+		{
+			LoadDataSource();
+		}
 
 #if EF7
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -29,57 +29,58 @@ namespace Microsoft.OData.Service.Sample.Northwind.Models
             base.OnConfiguring(optionsBuilder);
         }
 #else
-        public NorthwindContext()
-            : base("name=NorthwindConnection")
-        {
-        }
+		public NorthwindContext()
+				: base("name=NorthwindConnection")
+		{
+		}
 #endif
 
-        public virtual DbSet<Category> Categories { get; set; }
-        public virtual DbSet<Contact> Contacts { get; set; }
-        public virtual DbSet<CustomerDemographic> CustomerDemographics { get; set; }
-        public virtual DbSet<Customer> Customers { get; set; }
-        public virtual DbSet<Employee> Employees { get; set; }
-        public virtual DbSet<Order_Detail> Order_Details { get; set; }
-        public virtual DbSet<Order> Orders { get; set; }
-        public virtual DbSet<Product> Products { get; set; }
-        public virtual DbSet<Region> Regions { get; set; }
-        public virtual DbSet<Shipper> Shippers { get; set; }
-        public virtual DbSet<Supplier> Suppliers { get; set; }
-        public virtual DbSet<Territory> Territories { get; set; }
+		public virtual DbSet<Category> Categories { get; set; }
+		public virtual DbSet<Contact> Contacts { get; set; }
+		public virtual DbSet<CustomerDemographic> CustomerDemographics { get; set; }
+		public virtual DbSet<Customer> Customers { get; set; }
+		public virtual DbSet<Employee> Employees { get; set; }
+		public virtual DbSet<Order_Detail> Order_Details { get; set; }
+		public virtual DbSet<Order> Orders { get; set; }
+		public virtual DbSet<Product> Products { get; set; }
+		public virtual DbSet<Region> Regions { get; set; }
+		public virtual DbSet<Shipper> Shippers { get; set; }
+		public virtual DbSet<Supplier> Suppliers { get; set; }
+		public virtual DbSet<Territory> Territories { get; set; }
 
-        public void ResetDataSource()
-        {
+		public void ResetDataSource()
+		{
 #if EF7
             Database.EnsureDeleted();
 #else
-            if (Database.Exists())
-            {
-                Database.Delete();
-            }
+			if (Database.Exists())
+			{
+				Database.Delete();
+			}
 #endif
-            LoadDataSource();
-        }
+			LoadDataSource();
+		}
 
-        private static void LoadDataSource()
-        {
-            var dbPath = SqlLoader.GetDatabaseDirectory(null);
-            var loader = new SqlLoader();
-            loader.SetDatabaseEngine("(localdb)\\MSSQLLocalDB");
-            loader.AddScript("instnwdb.sql");
-            loader.AddScriptArgument("SqlSamplesDatabasePath", dbPath);
+		private static void LoadDataSource()
+		{
+			var dbPath = SqlLoader.GetDatabaseDirectory(null);
+			var loader = new SqlLoader();
+			loader.SetDatabaseEngine("(localdb)\\MSSQLLocalDB");
+			loader.AddScript("instnwdb.sql");
+			loader.AddScriptArgument("SqlSamplesDatabasePath", dbPath);
 
-            // Length of database name in SQLServer cannot exceed 128.
-            var dbNamePrefix = dbPath;
-            if (dbNamePrefix.Length > 100)
-            {
-                dbNamePrefix = dbNamePrefix.Substring(dbNamePrefix.Length - 100);
-                dbNamePrefix = dbNamePrefix.Substring(dbNamePrefix.IndexOf('\\') + 1);
-            }
+			// Length of database name in SQLServer cannot exceed 128.
+			var dbNamePrefix = dbPath;
+			if (dbNamePrefix.Length > 100)
+			{
+				dbNamePrefix = dbNamePrefix.Substring(dbNamePrefix.Length - 100);
+				dbNamePrefix = dbNamePrefix.Substring(dbNamePrefix.IndexOf('\\') + 1);
+				dbNamePrefix = dbNamePrefix.Replace("\\","_");
+			}
 
-            loader.AddScriptArgument("SqlDBNamePrefix", dbNamePrefix);
-            loader.Execute(dbPath);
-        }
+			loader.AddScriptArgument("SqlDBNamePrefix", dbNamePrefix);
+			loader.Execute(dbPath);
+		}
 
 #if EF7
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -134,75 +135,75 @@ namespace Microsoft.OData.Service.Sample.Northwind.Models
 
         }
 #else
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<CustomerDemographic>()
-                .Property(e => e.CustomerTypeID)
-                .IsFixedLength();
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<CustomerDemographic>()
+					.Property(e => e.CustomerTypeID)
+					.IsFixedLength();
 
-            modelBuilder.Entity<CustomerDemographic>()
-                .HasMany(e => e.Customers)
-                .WithMany(e => e.CustomerDemographics)
-                .Map(m => m.ToTable("CustomerCustomerDemo").MapLeftKey("CustomerTypeID").MapRightKey("CustomerID"));
+			modelBuilder.Entity<CustomerDemographic>()
+					.HasMany(e => e.Customers)
+					.WithMany(e => e.CustomerDemographics)
+					.Map(m => m.ToTable("CustomerCustomerDemo").MapLeftKey("CustomerTypeID").MapRightKey("CustomerID"));
 
-            modelBuilder.Entity<Customer>()
-                .Property(e => e.CustomerID)
-                .IsFixedLength();
+			modelBuilder.Entity<Customer>()
+					.Property(e => e.CustomerID)
+					.IsFixedLength();
 
-            modelBuilder.Entity<Employee>()
-                .HasMany(e => e.Employees1)
-                .WithOptional(e => e.Employee1)
-                .HasForeignKey(e => e.ReportsTo);
+			modelBuilder.Entity<Employee>()
+					.HasMany(e => e.Employees1)
+					.WithOptional(e => e.Employee1)
+					.HasForeignKey(e => e.ReportsTo);
 
-            modelBuilder.Entity<Employee>()
-                .HasMany(e => e.Territories)
-                .WithMany(e => e.Employees)
-                .Map(m => m.ToTable("EmployeeTerritories").MapLeftKey("EmployeeID").MapRightKey("TerritoryID"));
+			modelBuilder.Entity<Employee>()
+					.HasMany(e => e.Territories)
+					.WithMany(e => e.Employees)
+					.Map(m => m.ToTable("EmployeeTerritories").MapLeftKey("EmployeeID").MapRightKey("TerritoryID"));
 
-            modelBuilder.Entity<Order_Detail>()
-                .Property(e => e.UnitPrice)
-                .HasPrecision(19, 4);
+			modelBuilder.Entity<Order_Detail>()
+					.Property(e => e.UnitPrice)
+					.HasPrecision(19, 4);
 
-            modelBuilder.Entity<Order>()
-                .Property(e => e.CustomerID)
-                .IsFixedLength();
+			modelBuilder.Entity<Order>()
+					.Property(e => e.CustomerID)
+					.IsFixedLength();
 
-            modelBuilder.Entity<Order>()
-                .Property(e => e.Freight)
-                .HasPrecision(19, 4);
+			modelBuilder.Entity<Order>()
+					.Property(e => e.Freight)
+					.HasPrecision(19, 4);
 
-            modelBuilder.Entity<Order>()
-                .HasMany(e => e.Order_Details)
-                .WithRequired(e => e.Order)
-                .WillCascadeOnDelete(false);
+			modelBuilder.Entity<Order>()
+					.HasMany(e => e.Order_Details)
+					.WithRequired(e => e.Order)
+					.WillCascadeOnDelete(false);
 
-            modelBuilder.Entity<Product>()
-                .Property(e => e.UnitPrice)
-                .HasPrecision(19, 4);
+			modelBuilder.Entity<Product>()
+					.Property(e => e.UnitPrice)
+					.HasPrecision(19, 4);
 
-            modelBuilder.Entity<Product>()
-                .HasMany(e => e.Order_Details)
-                .WithRequired(e => e.Product)
-                .WillCascadeOnDelete(false);
+			modelBuilder.Entity<Product>()
+					.HasMany(e => e.Order_Details)
+					.WithRequired(e => e.Product)
+					.WillCascadeOnDelete(false);
 
-            modelBuilder.Entity<Region>()
-                .Property(e => e.RegionDescription)
-                .IsFixedLength();
+			modelBuilder.Entity<Region>()
+					.Property(e => e.RegionDescription)
+					.IsFixedLength();
 
-            modelBuilder.Entity<Region>()
-                .HasMany(e => e.Territories)
-                .WithRequired(e => e.Region)
-                .WillCascadeOnDelete(false);
+			modelBuilder.Entity<Region>()
+					.HasMany(e => e.Territories)
+					.WithRequired(e => e.Region)
+					.WillCascadeOnDelete(false);
 
-            modelBuilder.Entity<Shipper>()
-                .HasMany(e => e.Orders)
-                .WithOptional(e => e.Shipper)
-                .HasForeignKey(e => e.ShipVia);
+			modelBuilder.Entity<Shipper>()
+					.HasMany(e => e.Orders)
+					.WithOptional(e => e.Shipper)
+					.HasForeignKey(e => e.ShipVia);
 
-            modelBuilder.Entity<Territory>()
-                .Property(e => e.TerritoryDescription)
-                .IsFixedLength();
-        }
+			modelBuilder.Entity<Territory>()
+					.Property(e => e.TerritoryDescription)
+					.IsFixedLength();
+		}
 #endif
-    }
+	}
 }
